8bf6004a114488993f54715ae51c4fd37d7c804d
beskridge <beskridge@snu.edu>
2014-03-11 22:58:50 -0500

    Added copyright license to source files.

--- scripts/analysis/breakpoint/gather-all-emergence-times.sh
+++ scripts/analysis/breakpoint/gather-all-emergence-times.sh
@@ -6,7 +6,7 @@ ROOT_DIR=$1
 
 
 # =========================================================
-for INDCOUNT in 10 15 20 25 30 40 50 60
+for INDCOUNT in 10 15 20 25 30 40 50 60 70 80 90 100
 do
     echo Processing indcount [$INDCOUNT]
 
