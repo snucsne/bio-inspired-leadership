302028fe5e29bd99e9b3019727507f8d55c7bc4a
Tim Solum <timothy.solum@gmail.com>
2014-03-25 16:05:28 -0500

    Added Headers to Discrete Files
    Added Headers to Discrete Files

--- src/edu/snu/leader/discrete/simulator/SimulationState.java
+++ src/edu/snu/leader/discrete/simulator/SimulationState.java
@@ -1,3 +1,22 @@
+/*
+ *  The Bio-inspired Leadership Toolkit is a set of tools used to
+ *  simulate the emergence of leaders in multi-agent systems.
+ *  Copyright (C) 2014 Southern Nazarene University
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.snu.leader.discrete.simulator;
 
 import java.text.DateFormat;
@@ -30,8 +49,8 @@ import edu.snu.leader.discrete.utils.Reporter;
 public class SimulationState
 {
     final boolean SHOULD_REPORT_ESKRIDGE = false;
-    final boolean SHOULD_REPORT_CONFLICT = false;
-    final boolean SHOULD_REPORT_POSITIONS = true;
+    final boolean SHOULD_REPORT_CONFLICT = true;
+    final boolean SHOULD_REPORT_POSITIONS = false;
     
     /** Used for the Eskridge reporter */
     private final String SPACER = "=========================================================";
--- src/edu/snu/leader/discrete/utils/DestinationBuilder.java
+++ src/edu/snu/leader/discrete/utils/DestinationBuilder.java
@@ -1,3 +1,22 @@
+/*
+ *  The Bio-inspired Leadership Toolkit is a set of tools used to
+ *  simulate the emergence of leaders in multi-agent systems.
+ *  Copyright (C) 2014 Southern Nazarene University
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package edu.snu.leader.discrete.utils;
 
 import java.awt.geom.Point2D;
@@ -33,8 +52,8 @@ public class DestinationBuilder
         for( int count = 10; count <= 70; count += 10 )
         {
             DestinationBuilder db = new DestinationBuilder( count, 1 );
-            db.generatePoles( 50, 100, .50 );
-            db.generateSplitNorth( 150, 72, .50 );
+            db.generatePoles( 50, 100, 1.0 );
+            db.generateSplitNorth( 150, 72, 1.0 );
             db.generateSides( 25, 50, .50 );
 
             double[] horizontalPercentages = { 1 };
@@ -43,7 +62,7 @@ public class DestinationBuilder
             db.generateCircle( 55, count );
 
             db.generateOneNorth( 70 );
-            db.generateDifferentDistance(.5, 200, 100, 75);
+            db.generateDifferentDistance(0, 200, 100, 75);
         }
     }
 
